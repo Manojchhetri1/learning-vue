7987d50acb3f81ccb55bee0689ac455769f3883f
Manojchhetri1 <manoj.c@shikhartech.com>
2022-11-29 14:40:38 +0545

    learning conditional statament

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
 <div>{{greet}} {{name}}</div>
-<div v-text="company"></div>
+<div v-html="company"></div>
+<div v-html="link"></div>
+<h2 v-bind:id="headingId">Heading</h2>
+<button v-bind:disabled="isDisabled">Bind</button>
+<h2 class="underline">UnderLine Text</h2>
+<h2 class="underline" v-bind:class="status">Status</h2>
+<h2 v-bind:class="isCondition && 'condition'">Apply Condition</h2>
+<h2 v-bind:class="isOpen ? 'open': 'closed'">Ticket Status?</h2>
+<h2 v-bind:class="['open','condition']">Open and conditioned</h2>
+<h2 v-bind:class="[isCondition && 'condition', isOpen ? 'open': 'closed']">Array Conditional Line</h2>
+<h2 v-bind:class="{
+  condition: isCondition,
+  open: !isOpen,
+  'isOpen': isOpen
+}">Object Conditional Line</h2>
 </template>
 
 <script>
@@ -10,7 +24,13 @@ export default {
     return {
       greet:"Hello",
       name: "Manoz",
-      company:'ShikharTech',
+      company:'<b>ShikharTech</b>',
+      link:`<a href='test' onClick="alert('you are welcome')"> Click Here!</a>`,
+      headingId: 'heading',
+      isDisabled: false,
+      status:'success',
+      isCondition:true,
+      isOpen:false
     };
   },
 };
@@ -24,5 +44,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
+}
+
+.condition{
+  font-style: italic;
+}
+
+.open{
+  color: green;
+}
+
+.closed{
+  color: red;
 }
 </style>
